7e033951962559cfb06f4576efbd13c5bb573165
kulikov.a <28kulikov@mail.ru>
2022-04-28 14:33:03 +0300

    app_users

--- pet_shelter_project/pet_shelter_project/settings.py
+++ pet_shelter_project/pet_shelter_project/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/'
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
